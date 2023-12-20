0e6ccc9e753d531a8a451ad01cd36c8175ae063e
Nick Chernock <n.chernock@gmail.com>
2023-12-19 21:19:45 -0500

    functioning member count + started structure of api playerBar

--- scripts/clanViewer.js
+++ scripts/clanViewer.js
@@ -3,20 +3,16 @@
 let mainPage = document.querySelector("#mainPage");
 let totalMembers = document.querySelector("#totalMemb");
 let addClanMemberBtn = document.querySelector("#addClanMemberBtn");
-let deleteBtns = document.getElementsByClassName("delete");
-
-let countValue = deleteBtns.length;
 
 addClanMemberBtn.addEventListener('click', addPlayerBar);
 
 function addPlayerBar() {
-
-    // each playerBar has to be different, 
-        // what if we put copies in an array? then append the array?
     
     let playerList = document.querySelector("#playerList");
     let clanMember = document.createElement("li");
     let searchBar = document.createElement("input");
+    let memberCount = document.getElementById("memberCount");
+
     // button container (addSpan + deleteSpan)
     let btnContainer = document.createElement("div");
     let addSpan = document.createElement("span");
@@ -24,11 +20,13 @@ function addPlayerBar() {
     addSpan.textContent = "+";
     deleteSpan.textContent = "-";
     
-    // attach elements
+    // append elements
     btnContainer.append(addSpan);
     btnContainer.append(deleteSpan);
     clanMember.append(searchBar);
     clanMember.append(btnContainer);
+    playerList.appendChild(clanMember);  
+    totalMembers.append(memberCount);
 
     // class names
     addSpan.className = "plus";
@@ -37,39 +35,43 @@ function addPlayerBar() {
     clanMember.className = "clanMember";
 
     // search bar 
+    searchBar.className = "searchBar";
     searchBar.type = "text";
     searchBar.placeholder = "Player Name";
     searchBar.style.borderRadius = "5px";
     searchBar.style.paddingLeft = "5px";
 
+    let deleteBtns = [...document.getElementsByClassName('delete')];
+    let countValue = deleteBtns.length;
 
-    let memberCount = document.getElementById("memberCount");
-
-    // need countValue to 'refresh' everytime 
-    //          something is added/deleted
-    
-    memberCount.innerHTML = ++countValue;
-    
-    // console.log(countValue);
-
-    playerList.appendChild(clanMember);  
-    totalMembers.append(memberCount);
-
-    for (let i = 0; i < deleteBtns.length; ++i) {
+    // Click on a close button to hide the current list item
+    // reverse for loop because deleting items may cause the loop to bug iterations
+    for (let i = deleteBtns.length-1; i >= 0; --i) {
         deleteBtns[i].addEventListener('click', function() {
-            // need to actually remove the elements 
-            //      rather than hide them
-
-            // apparently this isnt actually an array 
-            //      and splice will never work?
-            deleteBtns
             let div = this.parentElement;
             let playerBar = div.parentElement;
-            playerBar.style.display = "none"; 
+            // actually remove the element
+            playerBar.remove(); 
+            // update countValue to reflect removal
+            countValue = countValue - 1;
+            memberCount.innerHTML = countValue;
+        });
+    }
+    memberCount.innerHTML = countValue;
+
+
+    while (searchBar.selected = true) {
+        searchBar.addEventListener('keypress', function(event) {
+            if(event.key === "Enter") {
+                addSpan.click();
+            }
         })
+        break;
     }
 
 }
+
+addClanMember();
 
 function addClanMember() {
     /*
@@ -78,21 +80,69 @@ function addClanMember() {
         - hooked up to API 
         - Add to total member count
     */
+
+    
+        let playerList = document.querySelector("#playerList");
+        let playerBar = document.createElement('li');
+        let playerName = document.createElement('span');
+        playerName.innerText = "Test"
+
+        let accordionDiv = document.createElement('div');
+        accordionDiv.className = "accordion";
+        let accordionItemDiv = document.createElement('div')
+        accordionItemDiv.className = "accordion-item";
+        let accordionHeader = document.createElement('h2');
+        accordionHeader.className = "accordion-header";
+        let accordionButton = document.createElement('button');
+        accordionButton.className = "accordion-button";
+
+        let accordionText = document.createElement('div');
+        accordionText.className = "accordion-collapse collapse show";
+        accordionText.id = "accordionText";
+        let accordionBody = document.createElement('div');
+        accordionBody.className = "accordion-body";
+
+        playerBar.append(accordionDiv);
+            accordionDiv.append(accordionItemDiv);
+                accordionItemDiv.append(accordionHeader);
+                    accordionHeader.append(accordionButton);
+                        accordionButton.append(playerName); 
+                accordionItemDiv.append(accordionText);
+                    accordionText.append(accordionBody);
+
+        accordionButton.dataset.bsTarget = "#accordionText"
+        accordionButton.dataset.bsToggle = "collapse";
+
+
+
+
+        // <div class="accordion" id="accordionPanelsStayOpenExample">
+        // <div class="accordion-item">
+        //   <h2 class="accordion-header">
+        //     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+        //       Accordion Item #1
+        //     </button>
+        //   </h2>
+        //   <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+        //     <div class="accordion-body">
+        //       <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        //     </div>
+        //   </div>
+        // </div>
+      
+
+        playerBar.className = "clanMember"
+        playerList.append(playerBar);
+
+
+
 }
 
-// pressing enter key acts as 'add' click
-// while (searchBar.selected = true) {
-//     searchBar.addEventListener('keypress', function(event) {
-//         if(event.key === "Enter") {
-//             // addSpan.click();
-//         }
-//     })
-//     break;
-// }
+// **pressing enter key acts as 'add' click**
 
 
 
-// Click on a close button to hide the current list item
+
 
 
 
